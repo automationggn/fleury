e2da738b71bee90d91b1543c090006c791c98599
adilkhan4 <adilkhan.ssitm@gmail.com>
2026-02-12 14:17:09 +0530

    Register HTTP functions for Azure Functions v4

--- api/src/index.js
+++ api/src/index.js
@@ -1,2 +1,16 @@
-require("./functions/enroll");
-require("./functions/verify");
+const { app } = require('@azure/functions');
+
+const enroll = require('./functions/enroll');
+const verify = require('./functions/verify');
+
+app.http('enroll', {
+  methods: ['POST'],
+  authLevel: 'anonymous',
+  handler: enroll
+});
+
+app.http('verify', {
+  methods: ['POST'],
+  authLevel: 'anonymous',
+  handler: verify
+});
